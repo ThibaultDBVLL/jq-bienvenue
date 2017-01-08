235d3b7e2c4ca681ca2950b78d08af7f9129fac5
ThibaultDBVLL <thibault.daboville@gmail.com>
2017-01-08 23:32:32 +0100

    Travail rendu - version définitive

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log('have fun !');
+
+/*Je pense avoir compris le principe. 
+Mais je n'arrive juste pas à l'appliquer. 
+Il est bientôt minuit, mon cerveau 
+est en grève*/
+
+
+$('input').bind('keyup', function(e) {
+    var input = $(this).val();
+    if (e.which == 13) {
+        $(this).val()
+    }
+})
+
+
+
+
+/*var prenom = $("#first_name").val();
+var ndf = $("last_name");
+var ville = $("city");
+
+
+console.log(prenom);  */
